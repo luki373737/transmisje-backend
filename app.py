663ef622c0d2d8867844b6134da49593127d10b5
luki373737 <78243665+luki373737@users.noreply.github.com>
2025-04-09 21:56:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -69,3 +69,8 @@ def get_transmissions():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
